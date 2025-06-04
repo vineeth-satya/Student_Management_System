a3f615eaad8181b6560467329c98286136b2cf7c
Satya_Vineeth <satyavineeth123@gmail.com>
2025-06-04 11:30:06 +0530

    Create RecordApp.java

---
+++ src/main/java/com/example/record/RecordApp.java
@@ -0,0 +1,10 @@
+package com.example.record;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class RecordApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RecordApp.class, args);
+    }
+}
+
